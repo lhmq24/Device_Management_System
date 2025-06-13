48f77a7e89866468f4f3735c025d48046b6f8e55
LeHoangMinhQuan <quanb2206008@student.ctu.edu.vn>
2025-06-13 09:59:36 +0700

    feat: initialize backend API with Express, add rate limiting and JSend response format
    - Created package.json with necessary dependencies and scripts.
    - Added server.js to start the Express server.
    - Implemented app.js to set up middleware, including CORS and rate limiting.
    - Introduced jsend.js for standardized API response format.
    - Configured Swagger UI for API documentation.

---
+++ backend-api/src/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const jsend = require("./jsend");
+const rateLimit = require("express-rate-limit");
+
+// const contactsRouter = require("./routes/contacts.router");
+// const {
+//   resourceNotFound,
+//   handleError,
+// } = require("./controllers/errors.controller");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../docs/openapiSpec.json");
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  limit: 20,
+  message: jsend.fail({
+    message: "Too many requests, please try again later.",
+  }),
+  statusCode: 429,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const app = express();
+app.use(limiter);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  return res.json(jsend.success());
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/public", express.static("public"));
+
+// contactsRouter.setup(app);
+
+// Handle 404 error for unknown URL paths
+// app.use(resourceNotFound);
+
+// Define the centralized error handling middleware, after all routes
+// and middlewares have been defined.
+// app.use(handleError);
+
+module.exports = app;
