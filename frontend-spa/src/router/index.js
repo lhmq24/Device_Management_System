e26f7cff47eb2e41c9cb6703f3b11a1d45469da7
PPT <thanhb2206015@student.ctu.edu.vn>
2025-07-21 22:48:19 +0700

    Config index.js for router

---
+++ frontend-spa/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import UnitsView from '../views/UnitsView.vue'
+import DevicesView from '../views/DevicesView.vue'
+import MaintainersView from '../views/MaintainersView.vue'
+import ReportsView from '../views/ReportsView.vue'
+
+const routes = [
+  { path: '/', redirect: '/units' },
+  { path: '/units', component: UnitsView },
+  { path: '/devices', component: DevicesView },
+  { path: '/maintainers', component: MaintainersView },
+  { path: '/reports', component: ReportsView },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
