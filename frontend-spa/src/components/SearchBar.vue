a4b77bd224c718e431e4a02df138dce684fcc8ed
PPT <thanhb2206015@student.ctu.edu.vn>
2025-07-21 23:02:06 +0700

    Added a SearchBar Bootstrap

---
+++ frontend-spa/src/components/SearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <input type="text" class="form-control" placeholder="Search units..." @input="handleInput" />
+</template>
+
+<script setup>
+const emit = defineEmits(['search'])
+
+function handleInput(event) {
+  emit('search', event?.target?.value || '')
+}
+</script>
